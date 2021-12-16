8bf3d24e7ccad866ea1ca0335137681aceec2620
Quentin <qmgermain@gmail.com>
2021-12-16 11:14:29 +0100

    ajout de la page composition dans le routage

--- speedsandwich/src/app/app-routing.module.ts
+++ speedsandwich/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AccueilComponent } from './controllers/accueil.component';
-import { PageNonExistanteComponent } from './controllers/page-non-existante.component';
+
 import { PanierComponent } from './controllers/panier.component';
 import { RechercheComponent } from './controllers/recherche.component';
+import { CompositionComponent } from './controllers/composition.component';
+import { PageNonExistanteComponent } from './controllers/page-non-existante.component';
 
-const routes: Routes = [
+const routes:Routes = [
   { path: '', component: AccueilComponent },
   { path: 'panier', component: PanierComponent },
-  { path:'search', component: RechercheComponent },
+  { path: 'search', component: RechercheComponent },
+  { path: 'compose', component: CompositionComponent },
   { path: '**', component: PageNonExistanteComponent }
 ];
 
