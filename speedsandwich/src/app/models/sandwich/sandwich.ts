618f1fc5515d68df3a493ae41c8b5b40a494a37d
ChantonyTY <74354624+ChantonyTY@users.noreply.github.com>
2021-12-17 14:24:47 +0100

    Update (prix du panier)
    Le prix s'afficher mal (trop de chiffre après la virgule)

--- speedsandwich/src/app/models/sandwich/sandwich.ts
+++ speedsandwich/src/app/models/sandwich/sandwich.ts
@@ -39,31 +39,6 @@ export class Sandwich {
 
     toString():string{
         var final : string = "";
-        /*this.ingredients!.forEach(element => {
-          let movie2 : any = Object.assign({}, element);
-          //console.log(movie2.count);
-          if(element.getCount()>=0){
-           if(a.includes(element.getLibelle())==false)
-              {
-                count=1;
-                movie2.count=count; 
-                  a+=element.getLibelle() + " " + movie2.count +"\n";
-
-                console.log(this.hasNumber(a))
-              }else{
-                count++;
-                movie2.count=count; 
-                if(this.hasNumber(a)==true){
-                  let str : string = "";
-                  str = a.substr(a.indexOf(element.getLibelle()),element.getLibelle().length+5)
-                  a = a.replace(str,"");
-                }       
-                let isnum = /^\d+$/.test(a);  
-                console.log(isnum);  
-                a+= element.getLibelle() + " " + movie2.count  +"\n";
-              }       
-          } 
-        });*/
         final+= "\n" + this.name  +' : pour le prix de ' + this.getPrix().toFixed(2) + ' €';
         final.replace("\n", "<br>");
         return final;
